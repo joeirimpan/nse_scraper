9c7cb50e803c5005f007747c2f43bc6b9a1dcefb
Joe Paul <joe.paul@fulfil.io>
2017-08-08 13:19:57 +0530

    Display top gainers and losers cards
    * Exposes index page on the root
    * Use underscore JS to inject html stock templates from ajax response

--- nse_scraper/app.py
+++ nse_scraper/app.py
@@ -5,12 +5,23 @@
     :copyright: (c) 2017 by Joe Paul.
     :license: see LICENSE for details.
 """
+import os
 import json
 
 import cherrypy
 from cherrypy.process.plugins import BackgroundTask
 
 from .scraper import NSEScraper
+
+
+def fp(rel_path):
+    "Return the full path of given rel_path"
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            rel_path
+        )
+    )
 
 
 class App(object):
@@ -26,6 +37,10 @@ class App(object):
             bus=cherrypy.engine
         ).start()
 
+    @cherrypy.expose
+    def index(self):
+        return file(fp('templates/index.html'))
+
     @cherrypy.expose(['stocks_info.json'])
     @cherrypy.tools.json_out()
     def get_stocks_info(self):
@@ -39,9 +54,17 @@ class App(object):
 
 if __name__ == '__main__':
     app = App()
+    config = {
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': fp('static'),
+            'tools.staticdir.index': 'index.html'
+        }
+    }
     cherrypy.tree.mount(
         root=app,
         script_name='/',
+        config=config
     )
     # Lets start the cherrypy engine so everything works
     cherrypy.engine.start()
